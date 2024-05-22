269cc6d5cd7a09e68abb29aaf3576f7f3ab6f700
paulvy <paulvyid@gail.com>
2024-05-22 12:51:22 +0100

    Case Study : Lloyds : Demo Project

--- src/main/java/com/lloyds/azure/lloydsazure/paymentController/PaymentControllerProcess.java
+++ src/main/java/com/lloyds/azure/lloydsazure/paymentController/PaymentControllerProcess.java
@@ -3,7 +3,6 @@ package com.lloyds.azure.lloydsazure.paymentController;
 import com.lloyds.azure.lloydsazure.paymentServices.PaymentConsumer;
 import com.lloyds.azure.lloydsazure.paymentsModel.Payments;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
